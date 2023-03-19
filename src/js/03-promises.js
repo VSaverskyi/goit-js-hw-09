eeb0d950cf72a064afa3b9223090a265e20a78e8
VSaverskyi <vovikck15@gmail.com>
2023-03-19 17:05:34 +0100

    fixed setInterval

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,34 +7,34 @@ refs.form.addEventListener('submit', e => {
   const {
     elements: { delay, step, amount },
   } = e.currentTarget;
-  let position = 1;
+  let position = 0;
   let numberValue = Number(delay.value);
   let intervalId = setInterval(() => {
+    position += 1;
     createPromise(position, numberValue)
       .then(({ position, delay }) => {
         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      })
-      .finally(() => {
-        if (position >= Number(amount.value)) {
-          clearInterval(intervalId);
-        }
-        numberValue += Number(step.value);
-        position += 1;
       });
+    numberValue += Number(step.value);
+    if (position === Number(amount.value)) {
+      clearInterval(intervalId);
+    }
   }, numberValue);
 });
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      // Fulfill
-      resolve({ position, delay });
-    }
-    // Reject
-    reject({ position, delay });
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      }
+      // Reject
+      reject({ position, delay });
+    }, delay);
   });
 }
